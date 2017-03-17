be0f7edb73350b8c23c1222259809201e65a1434
Manuel Romero <manuel.romero@qlik.com>
2017-03-17 11:53:26 +0000

    display measure value

--- libs/routes/interact.js
+++ libs/routes/interact.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var botLogic = require('../bot-logic.js');
+var logger = require('../logger.js');
 
 router.get('/ping', function ( req, res ) {
    res.send("pong");
@@ -8,6 +9,7 @@ router.get('/ping', function ( req, res ) {
 
 router.post('/', function ( req, res ) {
     var payload = JSON.parse(req.body.payload);
+    logger.log().info( "interact payload", payload );
     if (payload.callback_id === "select_app"){
     	botLogic.setActiveApp( payload.actions[0], payload.user.name );
     }
